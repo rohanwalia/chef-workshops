114e024760422da8af660f4ba5aa5ba07b84f030
rohanwalia <er.walia@gmail.com>
2018-06-13 15:36:05 +0000

    Replaced default.rb with install.rb

--- 1_MongoDB/mongod_cookbook/recipes/install.rb
+++ 1_MongoDB/mongod_cookbook/recipes/install.rb
@@ -5,10 +5,10 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
   template '/etc/yum.repos.d/mongodb-org-3.6.repo' do
-    source 'mongod_yum.erb'  
+    source 'mongod_yum.erb'
   end
-    
-  package 'mongodb-org'  
+
+  package 'mongodb-org'
     service 'mongod' do
-    action [:enable, :start]  
+    action [:enable, :start]
   end
