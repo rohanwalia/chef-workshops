6adbe6503f5bd990691ec0d390c6407b6500fb83
rohanwalia <er.walia@gmail.com>
2018-06-13 13:31:25 +0000

    Added recipe for MONGOD

--- 1_MongoDB/mongod_cookbook/recipes/default.rb
+++ 1_MongoDB/mongod_cookbook/recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+  template '/etc/yum.repos.d/mongodb-org-3.6.repo' do
+    source 'mongod_yum.erb'  
+  end
+    
+  package 'mongodb-org'  
+    service 'mongod' do
+    action [:enable, :start]  
+  end
