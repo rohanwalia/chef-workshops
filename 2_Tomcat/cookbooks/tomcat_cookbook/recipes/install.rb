d1f0e55b3902757d4fb360418b48a57b713bc3d1
rohanwalia <er.walia@gmail.com>
2018-06-14 14:17:56 +0000

    Fixed recipe install and template files

--- 2_Tomcat/cookbooks/tomcat_cookbook/recipes/install.rb
+++ 2_Tomcat/cookbooks/tomcat_cookbook/recipes/install.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 # Install java-1.7.0-openjdk-devel - sudo yum install java-1.7.0-openjdk-devel
-package java-1.7.0-openjdk-devel
+package 'java-1.7.0-openjdk-devel'
 
 #Add Group tomcat
 group 'tomcat'
@@ -18,38 +18,40 @@ user 'tomcat' do
     home '/opt/tomcat'
   end
 
-  #Create Directory /opt/tomcat
-  directory '/opt/tomcat' do
-    group 'tomcat'
-  end
-
   #Download and extract tomcat 8 - $ wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz
  remote_file 'apache-tomcat-8.5.20.tar.gz' do
     source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.9/bin/apache-tomcat-8.5.9.tar.gz'
  end
 
+ #Create Directory /opt/tomcat
+ directory '/opt/tomcat' do
+   group 'tomcat'
+ end
+
  execute 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
 
- execute 'chgrp -R tomcat /opt/tomcat/conf'
+ execute 'chgrp -R tomcat /opt/tomcat/'
 
- directory '/opt/tomcat/conf' do
-    mode '0070'
- end
+ execute 'chmod -R g+r /opt/tomcat/conf/'
  
- execute 'chmod g+r /opt/tomcat/conf/*'
+ execute 'chmod g+x /opt/tomcat/conf/'
+ 
+ execute 'chown -R tomcat /opt/tomcat/'
 
- execute 'chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/logs/ /opt/tomcat/temp/'
+ #execute 'chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/logs/ /opt/tomcat/temp/'
 
  #Install the Systemd Unit File
   template '/etc/systemd/system/tomcat.service' do
     source 'tomcat.service.erb'
   end
 
-  systemd_unit 'tomcat.service' do
-    action :reload
-    end
+#  systemd_unit 'tomcat.service' do
+ #   action :reload
+#    end
+  
+  execute 'systemctl daemon-reload'
 
   service 'tomcat' do
-    action [:enable, :start]
+    action [:start, :enable]
   end
 
