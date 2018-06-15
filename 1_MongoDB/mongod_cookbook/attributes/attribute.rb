f8fde34b1c9aac71bd19260b13cccc0ef10f8bba
rohanwalia <er.walia@gmail.com>
2018-06-15 12:48:30 +0000

    parameterized attributes

---
+++ 1_MongoDB/mongod_cookbook/attributes/attribute.rb
@@ -0,0 +1,1 @@
+default['mongod_recipe']['package_update_command'] = 'yum -y update'
--- 1_MongoDB/mongod_cookbook/recipes/install.rb
+++ 1_MongoDB/mongod_cookbook/recipes/install.rb
@@ -4,6 +4,8 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+  execute node.default['mongod_recipe']['package_update_command']
+
   template '/etc/yum.repos.d/mongodb-org-3.6.repo' do
     source 'mongod_yum.erb'
   end
