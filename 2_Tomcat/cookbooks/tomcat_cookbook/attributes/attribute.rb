07163c6c07b22eb145b5895b407928a54b0e451d
rohanwalia <er.walia@gmail.com>
2018-06-16 14:04:30 +0000

    added funcationality for recipe to work for both Ubuntu 16 and RHEL 7.5

--- 2_Tomcat/cookbooks/tomcat_cookbook/attributes/attribute.rb
+++ 2_Tomcat/cookbooks/tomcat_cookbook/attributes/attribute.rb
@@ -5,7 +5,6 @@ default['tomcat_recipe']['tomcat_dir'] = '/opt/tomcat'
 default['tomcat_recipe']['tomcat_conf_dir'] = '/opt/tomcat/conf/'
 default['tomcat_recipe']['tomcat_service_unit'] = '/etc/systemd/system/tomcat.service'
 default['tomcat_recipe']['tomcat_repo_source'] = 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.9/bin/apache-tomcat-8.5.9.tar.gz'
-default['tomcat_recipe']['tomcat_service_unit_template'] = 'tomcat.service.erb'
 default['tomcat_recipe']['tomcat_service'] = 'tomcat'
 default['tomcat_recipe']['tomcat_service_daemon'] = 'daemon'
 default['tomcat_recipe']['nologin_shell'] = '/bin/nologin'
@@ -18,9 +17,11 @@ case node['platform_family']
  when 'rhel'
   default['tomcat_recipe']['package_update_command'] = 'yum -y update'
   default['tomcat_recipe']['java_package'] = 'java-1.7.0-openjdk-devel'
+  default['tomcat_recipe']['tomcat_service_unit_template'] = 'tomcat.service.erb'
  when 'debian'
   default['tomcat_recipe']['package_update_command'] = 'apt-get update'
   default['tomcat_recipe']['java_package'] = 'default-jdk'
+  default['tomcat_recipe']['tomcat_service_unit_template'] = 'tomcat.service.ubuntu16.erb'
 end
 
 
