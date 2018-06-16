5023994d9ea78c3ec0ad083ead2805a4aa50cb6b
rohanwalia <er.walia@gmail.com>
2018-06-16 09:09:33 +0000

    Added attributes

--- 2_Tomcat/cookbooks/tomcat_cookbook/recipes/install.rb
+++ 2_Tomcat/cookbooks/tomcat_cookbook/recipes/install.rb
@@ -5,27 +5,35 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 # Install java-1.7.0-openjdk-devel - sudo yum install java-1.7.0-openjdk-devel
-package 'java-1.7.0-openjdk-devel'
+package node.default['tomcat_recipe']['java_package']
+
+## Variable assignments
+user = node.default['tomcat_recipe']['tomcat_user']
+group = node.default['tomcat_recipe']['tomcat_group']
+tomcat_dir = node.default['tomcat_recipe']['tomcat_dir']
+tomcat_conf_dir = node.default['tomcat_recipe']['tomcat_conf_dir']
+repo_source = node.default['tomcat_recipe']['tomcat_repo_source']
+
 
 #Add Group tomcat
-group 'tomcat'
+group "#{group}"
 
 #Add user tomcat with home directory
-user 'tomcat' do
+user "#{user}" do
     manage_home false
-    group 'tomcat'
+    group "#{group}"
     shell '/bin/nologin'
-    home '/opt/tomcat'
+    home "#{tomcat_dir}"
   end
 
   #Download and extract tomcat 8 - $ wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz
  remote_file 'apache-tomcat-8.5.20.tar.gz' do
-    source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.9/bin/apache-tomcat-8.5.9.tar.gz'
+    source "#{repo_source}"
  end
 
  #Create Directory /opt/tomcat
- directory '/opt/tomcat' do
-   group 'tomcat'
+ directory "#{tomcat_dir}" do
+   group "#{group}"
    recursive true
  end
 
@@ -39,8 +47,8 @@ user 'tomcat' do
 
 
  #Install the Systemd Unit File
-  template '/etc/systemd/system/tomcat.service' do
-    source 'tomcat.service.erb'
+  template node.default['tomcat_recipe']['tomcat_service_unit'] do
+    source node.default['tomcat_recipe']['tomcat_service_unit_template']
   end
 
   systemd_unit 'daemon' do
