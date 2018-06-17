956414556f4821bb78e5289226e68aa1f61556bb
rohanwalia <er.walia@gmail.com>
2018-06-17 02:28:01 +0000

    Added code for ubuntu

--- 1_MongoDB/mongod_cookbook/attributes/attribute.rb
+++ 1_MongoDB/mongod_cookbook/attributes/attribute.rb
@@ -1,5 +1,15 @@
-default['mongod_recipe']['package_update_command'] = 'yum -y update'
 default['mongod_recipe']['mongo_repo_file'] = '/etc/yum.repos.d/mongodb-org-3.6.repo'
 default['mongod_recipe']['mongod_package'] = 'mongodb-org'
 default['mongod_recipe']['mongod_service'] = 'mongod'
+default['mongod_recipe']['key_ubuntu1604_command'] = 'apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5'
 
+default['mongod_recipe']['mongopackage_ubuntu1604_command'] = 'echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list'
+
+case node['platform_family']
+ when 'rhel'
+ default['mongod_recipe']['package_update_command'] = 'yum -y update'
+
+ when 'debian'
+ default['mongod_recipe']['package_update_command'] = 'apt-get update'
+ default['mongod_recipe']['mongopackage_ubuntu1604_repo_file'] = '/etc/apt/sources.list.d/mongodb-org-3.6.list'
+end
--- 1_MongoDB/mongod_cookbook/recipes/install.rb
+++ 1_MongoDB/mongod_cookbook/recipes/install.rb
@@ -4,15 +4,37 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+  #execute package manager update.
   execute node.default['mongod_recipe']['package_update_command']
- 
+  
+  #Check for platform
+  case node['platform_family']
+   when 'debian' # do things if platform is debian based - ubuntu
 
-  template node.default['mongod_recipe']['mongo_repo_file'] do
-    source 'mongod_yum.erb'
+   execute'key' do
+    command node.default['mongod_recipe']['key_ubuntu1604_command'] 
+   end 
+
+   execute 'update_repo' do
+     command node.default['mongod_recipe']['mongopackage_ubuntu1604_command']
+     not_if do
+	File.exists?(node.default['mongod_recipe']['mongopackage_ubuntu1604_repo_file'])
+     end
+   end
+   execute node.default['mongod_recipe']['package_update_command']
+   
+   when 'rhel' # do things if platform is rhel based
+    
+    template node.default['mongod_recipe']['mongo_repo_file'] do
+     source 'mongod_yum.erb'
+   end
+
   end
 
+  #install mongod using package manager
   package node.default['mongod_recipe']['mongod_package']
 
+  #enable and start mongod service
   service node.default['mongod_recipe']['mongod_service'] do
     action [:enable, :start]
   end
